ab5822c98bd046b5aa86f5aecaf585783795c064
CupricWolf <marcello.s.nicoletti@gmail.com>
2017-11-09 01:29:27 -0700

    Added SSD constraint data structures.

---
+++ src/SsdConstraint.java
@@ -0,0 +1,68 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class SsdConstraint {
+    private final int n;
+    private final Set<RbacRole> roleSet;
+
+    /**
+     * Constructs a new constraint on a set of roles with a cardinality of N.
+     * A set of roles "passes" a constraint as long as less than N roles match.
+     * N = 2 is a mutually exclusive constraint as a roleSet may only have one
+     * from this constraint that matches to pass.
+     *
+     * @param n The Cardinality of the constraint.
+     * @param roleSet The set of roles under constraint.
+     */
+    public SsdConstraint (int n, Set<RbacRole> roleSet) {
+        if (n < 2) {
+            throw new IllegalArgumentException("N must be greater than or " +
+                    "equal to 2.");
+        }
+        this.n = n;
+        this.roleSet = roleSet;
+    }
+
+    /**
+     * Tests a set of roles against this constraint. Constraint is violated
+     * if N or more roles exist in this set.
+     *
+     * @param testRoles The set of roles to test.
+     * @return true if this constraint is satisfied by the passed roles.
+     */
+    public boolean test (Set<RbacRole> testRoles) {
+        Set<RbacRole> intersection = new HashSet<>(roleSet);
+        intersection.retainAll(testRoles);
+
+        return intersection.size() < n;
+    }
+
+    public int getN () {
+        return n;
+    }
+
+    public Set<RbacRole> getRoleSet () {
+        return roleSet;
+    }
+
+    @Override
+    public boolean equals (Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof SsdConstraint)) {
+            return false;
+        }
+
+        SsdConstraint that = (SsdConstraint)o;
+
+        return n == that.n && roleSet.equals(that.roleSet);
+    }
+
+    @Override
+    public int hashCode () {
+        int result = n;
+        result = 31 * result + roleSet.hashCode();
+        return result;
+    }
+}
---
+++ src/SsdConstraintSet.java
@@ -0,0 +1,45 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class SsdConstraintSet {
+    private Set<SsdConstraint> constraints;
+
+    /**
+     * Constructs a new empty set of constraints.
+     */
+    public SsdConstraintSet () {
+        this.constraints = new HashSet<>();
+    }
+
+    /**
+     * Adds a new constraint to this set.
+     *
+     * @param n The cardinality of the new constraint.
+     * @param roles The set the new constraint applies to.
+     * @return True if the constraint was added.
+     */
+    public boolean addConstraint (int n, Set<RbacRole> roles) {
+        return addConstraint(new SsdConstraint(n, roles));
+    }
+
+    /**
+     * Adds the constraint to this set.
+     *
+     * @param constraint The constraint to add.
+     * @return True if the constraint was added.
+     */
+    public boolean addConstraint (SsdConstraint constraint) {
+        return constraints.add(constraint);
+    }
+
+    /**
+     * Tests a set of roles against all constraints.
+     *
+     * @param roles The set of roles to test.
+     * @return True if set satisfies all constraints.
+     */
+    public boolean testAll (Set<RbacRole> roles) {
+        return constraints.stream().allMatch(constraint -> constraint.test
+                (roles));
+    }
+}
